c8d9532f3c9d92c1072c1ae14b0fb13fe9f00ebc
Antonio Braz <antoniovbraz@gmail.com>
2020-05-27 08:41:55 -0300

    alot of things working

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,38 @@
+class TasksController < ApplicationController
+before_action :set_task, only: [:show]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+end
